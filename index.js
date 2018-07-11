7e3694b4f97caa376821abd109ad8b9a91388506
syd2923 <sydloveskay@gmail.com>
2018-07-11 19:06:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // Write your code in this file!
+var currentUser = "sydney"
+var welcomeMessage = "Welcome to Klossybook," + currentUser +"!"
+var excitedWelcomeMessage = welcomeMessage.toUpperCase();
+var shortGreetingk
 
